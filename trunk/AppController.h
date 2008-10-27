2bf864c133cea4ef75105812f42a928d5f002b8e
dolzsan <dolzsan@f152e352-a425-11dd-ba4f-13945de82653>
2008-10-27 13:16:40 +0000

    Initial commit. Beware! Messy code...

---
+++ trunk/AppController.h
@@ -0,0 +1,71 @@
+//
+//  AppController.h
+//  Bistec
+//
+//  Created by Joan Dolz Sánchez on 08/10/08.
+//  Copyright 2008 Joan Dolz. All rights reserved.
+//
+
+/***************************************************************************
+
+This file is part of Bistec.
+
+Bistec is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bistec is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Bistec.  If not, see <http://www.gnu.org/licenses/>.
+
+***************************************************************************/
+
+#import <Cocoa/Cocoa.h>
+#import <QTKit/QTKit.h>
+
+
+@interface AppController : NSObject {
+	QTMovie * theMovie;
+	NSTimer * TCUpdateTimer;
+	
+	BOOL saved;
+	NSString * path;
+	
+	NSMutableArray * textList;
+	NSMutableArray * TCInList;
+	NSMutableArray * TCOutList;
+	
+	IBOutlet QTMovieView * movieCanvas;
+	IBOutlet NSTextField * TCLabel;
+	IBOutlet NSTextView * captionView;
+	IBOutlet NSTextField * TCInLabel;
+	IBOutlet NSTextField * TCOutLabel;
+	IBOutlet NSTextView * subtitleField;
+	IBOutlet NSTableView * subtitleList;
+}
+
+- (IBAction)loadMovie:(id)sender;
+- (IBAction)addSubtitle:(id)sender;
+- (IBAction)setTCIn:(id)sender;
+- (IBAction)setTCOut:(id)sender;
+- (IBAction)removeSubtitle:(id)sender;
+- (IBAction)saveFile:(id)sender;
+- (IBAction)openSRT:(id)sender;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification;
+- (void)updateTC:(id)sender;
+- (NSString *)TCForMilliseconds:(NSInteger)milliseconds;
+- (void)saveSrtTo:(char*)aPath;
+
+// delegate methods
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)aTableView;
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex;
+- (BOOL)tableView:(NSTableView *)aTableView shouldSelectRow:(NSInteger)rowIndex;
+- (void)textDidChange:(NSNotification *)aNotification;
+
+@end
